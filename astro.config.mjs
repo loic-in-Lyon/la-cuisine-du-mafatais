772a9fbf1bdeeb57d9691fb791b6222d93021899
Loïc POO-SING <130918150+loic-in-Lyon@users.noreply.github.com>
2026-09-20 21:44:10 +0200

    feat: ajout de l'integration sitemap pour le référencement Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
-  adapter: vercel()
+  site: 'https://lacuisinedumafatais.fr', // <--- Indiquez bien votre domaine principal ici
+  adapter: vercel(),
+  integrations: [sitemap()]
 });
